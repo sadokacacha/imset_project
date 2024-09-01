37a524f18d193b95ba0e19c37c20bca066fadbec
sadokacacha <sadokacacha1998@gmail.com>
2024-09-01 22:55:37 +0100

    adding home nav

--- front/app/components/Navbar.tsx
+++ front/app/components/Navbar.tsx
@@ -22,29 +22,43 @@ const Navbar: React.FC = () => {
           {user?.role === 'admin' && (
             <>
               <li>
-                <Link className="link" href="">
-                  Manage Users
+                <Link className="link" href="/admin/dashboard">
+                  Home
                 </Link>
               </li>
 
               <li>
-                <Link className="link" href="/admin/dashboard">
-                  view resume
+                <Link className="link" href="">
+                  Manage Users
                 </Link>
               </li>
             </>
           )}
 
           {user?.role === 'teacher' && (
-            <li>
-              <Link className="link" href="/teacher/dashboard/AddFiles">
-                add files
-              </Link>
-            </li>
+            <>
+              <li>
+                <Link className="link" href="/teacher/dashboard">
+                  HOME
+                </Link>
+              </li>
+
+              <li>
+                <Link className="link" href="/teacher/dashboard/AddFiles">
+                  add files
+                </Link>
+              </li>
+            </>
           )}
 
           {user?.role === 'student' && (
             <>
+              <li>
+                <Link className="link" href="/student/dashboard">
+                  HOME
+                </Link>
+              </li>
+    
               <li>
                 <Link className="link" href="/student/dashboard/ViewResume">
                   Resume
